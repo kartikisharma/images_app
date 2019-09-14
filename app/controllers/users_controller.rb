95d7e76e5af51705634c56a123b763ed7a39aaf6
Kartiki Sharma <k35sharm@uwaterloo.ca>
2019-09-13 23:16:27 -0400

    Configure User's controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.email.downcase!
+
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      flash.now.alert = "Oops, couldn't create account. Please use a valid email and password and try again."
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
