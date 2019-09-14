545ebbafeec29efe1a6830e6073e9c53960b36b2
Kartiki Sharma <k35sharm@uwaterloo.ca>
2019-09-14 00:11:37 -0400

    Add imageList

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authorize
+    redirect_to login_path, alert: 'You must be logged in to access this page.' if current_user.nil?
+  end
 end
