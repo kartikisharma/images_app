57de2d02d847c73b091998c7eb0c943376d41b5a
Kartiki Sharma <k35sharm@uwaterloo.ca>
2019-09-13 23:57:27 -0400

    Added session & state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    # ||= ensures helper doesn't hit the db every time a user hits a web page;
+    # it will look it up once, then cache it
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
